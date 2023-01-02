0854316b259bfb70c56fe8850066bcff8a9f79e2
Denis Ievlev <iievliievdenis@gmail.com>
2023-01-02 17:50:09 +0200

    add messages window

--- src/Components/Messages/Messages.js
+++ src/Components/Messages/Messages.js
@@ -1,9 +1,38 @@
+import styled from 'styled-components';
+
+
+const MessagesWrapper = styled.div `
+display: flex;
+justify-content:  space-between;
+width: 300px;
+`;
+
+const MessagesItems = styled.div ``;
+
+const Dialog = styled.div ``;
+
+const DialogMessages = styled.div ``;
+
+const DialogMessage = styled.div ``;
 
 const Messages = () => {
   return (
-    <div>
-      Messages
-    </div>
+    <MessagesWrapper>
+     <MessagesItems>
+      <Dialog>Roman</Dialog>
+      <Dialog>Arthur</Dialog>
+      <Dialog>Svetlana</Dialog>
+      <Dialog>Dmitry</Dialog>
+      <Dialog>Victoria</Dialog>
+     </MessagesItems>
+     <DialogMessages>
+     <DialogMessage>Hello, how are you?</DialogMessage>
+     <DialogMessage>Do you want to take a walk?</DialogMessage>
+     <DialogMessage>Where is my key, did you know?</DialogMessage>
+     <DialogMessage>See how it's cheap!</DialogMessage>
+     <DialogMessage>Going for a lunch, you with me?</DialogMessage>
+     </DialogMessages>
+    </MessagesWrapper>
   );
 }
 
